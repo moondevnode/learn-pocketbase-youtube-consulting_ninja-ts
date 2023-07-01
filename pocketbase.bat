4467877fe02b7ba28b56c319ba816db948b1d17a
undefined <moondevnode@gmail.com>
2023-07-02 07:44:27 +0900

    OAuth2 using Google, PocketBase, and SvelteKit

---
+++ pocketbase.bat
@@ -0,0 +1,1 @@
+cd C:/JnJ-soft/Developments/Database/sqlite && pocketbase serve --dir="./pb-consullting_ninja" --http="127.0.0.1:8090"
--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -1,26 +1,24 @@
 <nav>
   <a href="/">Home</a>
-  <a href="/create">Create</a>
-  <a href="/read">Read</a>
-  <a href="/update">Update</a>
-  <a href="/destroy">Destroy</a>
+  <a href="/signup">Sign Up</a>
+
 </nav>
 
 
 <style>
   nav{
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: center;
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      justify-content: center;
   }
   nav a{
-    margin: .5em;
-    color: #000000;
-    text-decoration: none;
-    font-weight: 800;
+      margin: .5em;
+      color: #000000;
+      text-decoration: none;
+      font-weight: 800;
   }
   nav a:hover{
-    text-decoration: underline;
+      text-decoration: underline;
   }
 </style>
